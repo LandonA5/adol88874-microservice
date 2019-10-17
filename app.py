817c7230ee87afe0cbaac067ad936166fa48f592
LandonA5 <landon.adolphson@gmail.com>
2019-10-17 00:55:54 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return render_template('1.html')
+   return 'Service is alive'
+
+@app.route('/1', methods=['GET', 'POST'])
+def post1(userNum):
+   if request.method == 'POST':
+       return userNum
+   else:
+       return 'You should POST to this URL'
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
