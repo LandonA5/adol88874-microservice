f9f89ddc3f9f14afc48d34f933ca0c25358176d3
LandonA5 <landon.adolphson@gmail.com>
2019-10-17 02:43:40 +0000

    new app.py

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
-from flask import Flask, render_template, request
+# Minimum viable microservice from 
+# https://towardsdatascience.com/working-with-apis-using-flask-flask-restplus-and-swagger-ui-7cf447deda7f
 
-app = Flask(__name__)
+from flask import Flask
+from flask_restplus import Api, Resource
 
-@app.route('/')
-def index():
-   return 'Service is alive'
+flask_app = Flask(__name__)
+app = Api(app = flask_app)
 
-@app.route('/1', methods=['GET', 'POST'])
-def post1():
-   if request.method == 'POST':
-       userNum = request.form['userNum']
-       return jsonify(userNum)
-   else:
-       return 'You should POST to this URL'
+name_space = app.namespace('main', description='Main APIs')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+@name_space.route("/")
+class MainClass(Resource):
+	def get(self):
+		return {
+			"status": "Got new data"
+		}
+	def post(self):
+		return {
+			"status": "Posted new data"
+		}
 
+if __name__ == '__main__':
+    flask_app.run(debug=True, host='0.0.0.0')
 
