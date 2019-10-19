c60171017b3253bd79e8b50414efadf9ddf69d86
LandonA5 <landon.adolphson@gmail.com>
2019-10-19 00:28:01 +0000

    Finalizing on the simpler service

--- app.py
+++ app.py
@@ -1,25 +1,11 @@
-# Minimum viable microservice from 
-# https://towardsdatascience.com/working-with-apis-using-flask-flask-restplus-and-swagger-ui-7cf447deda7f
-
 from flask import Flask
-from flask_restplus import Api, Resource
 
-flask_app = Flask(__name__)
-app = Api(app = flask_app)
+app = Flask(__name__)
 
-name_space = app.namespace('main', description='Main APIs')
-
-@name_space.route("/")
-class MainClass(Resource):
-	def get(self):
-		return {
-			"status": "Got new data"
-		}
-	def post(self):
-		return {
-			"status": "Posted new data"
-		}
+@app.route('/')
+def index():
+   return 'Service is alive'
 
 if __name__ == '__main__':
-    flask_app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0')
 
